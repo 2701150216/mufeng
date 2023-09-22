42d2fa589687e6147ea35a188408a3fff350dd63
2701150216 <37213175+2701150216@users.noreply.github.com>
2023-09-22 15:15:43 +0800

    Update AdminApplication.java

--- mogu_admin/src/main/java/com/moxi/mogublog/admin/AdminApplication.java
+++ mogu_admin/src/main/java/com/moxi/mogublog/admin/AdminApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
@@ -19,6 +20,7 @@ import java.util.TimeZone;
  * @author 陌溪
  * @date 2020年12月31日21:26:04
  */
+@EnableScheduling
 @EnableTransactionManagement
 @SpringBootApplication
 @EnableOpenApi
