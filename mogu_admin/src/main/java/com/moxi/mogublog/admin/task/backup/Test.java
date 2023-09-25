fdefcc6590195106694c2ec6a7e51bd837d024da
mufeng <mufeng@qq.com>
2023-09-25 17:01:01 +0800

    test action

--- mogu_admin/src/main/java/com/moxi/mogublog/admin/task/backup/Test.java
+++ mogu_admin/src/main/java/com/moxi/mogublog/admin/task/backup/Test.java
@@ -2,6 +2,6 @@ package com.moxi.mogublog.admin.task.backup;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test22222");
+        System.out.println("test2222211111");
     }
 }
